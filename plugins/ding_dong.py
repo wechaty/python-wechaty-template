63f3651c0d7a2441b8859487a04c6d71224f0238
骑马小猫 <1435130236@qq.com>
2022-07-09 08:33:02 +0000

    add basic files

---
+++ plugins/ding_dong.py
@@ -0,0 +1,65 @@
+"""basic ding-dong bot for the wechaty plugin"""
+import asyncio
+from typing import Optional
+from asyncio import Event
+from quart import Quart, jsonify
+
+from wechaty import Message, Wechaty, WechatyPluginOptions
+from wechaty.plugin import WechatyPlugin
+from wechaty_puppet import get_logger
+
+
+
+class DingDongPlugin(WechatyPlugin):
+    """DingDong Plugin"""
+    def __init__(self, options: Optional[WechatyPluginOptions] = None):
+        super().__init__(options)
+        
+        self.event = Event()
+        self.is_init = False
+        self.logger = get_logger('messages', file='.wechaty/messages.log')
+
+    async def init_plugin(self, wechaty: Wechaty) -> None:
+        wechaty.on('dong', self.on_dong)
+
+    async def on_dong(self, *args, **kwargs) -> None:
+        """listen dong event"""
+        if not self.is_init:
+            return
+        self.event.set()
+
+    @message_controller.may_disable_message
+    async def on_message(self, msg: Message) -> None:
+        """listen message event"""
+        talker = msg.talker()
+        text = msg.text()
+        if msg.room():
+            topic = await msg.room().topic()
+            if topic.startswith('嘉怡') and topic.endswith('号楼组群'):
+                return
+            if topic == '嘉怡志愿者群':
+                return
+
+            self.logger.info(msg)
+        self.logger.info(msg)
+
+        if text == 'ding':
+            message_controller.disable_all_plugins(msg)
+            if msg.room():
+                await msg.room().say('dong', mention_ids=[talker.contact_id])
+            else:
+                await talker.say('dong')
+
+    async def blueprint(self, app: Quart) -> None:
+        @app.route('/ding')
+        async def listence_ding():
+            if not self.is_init:
+                self.event._loop = asyncio.get_event_loop()
+                self.is_init = True
+            
+            await self.bot.puppet.ding()
+            if self.event.is_set():
+                self.event.clear()
+            await self.event.wait()
+            self.event.clear()
+            return jsonify(dict(code=200, msg='dong'))
